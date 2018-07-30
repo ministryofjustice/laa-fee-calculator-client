a816b4ebe88a529be4776616da2ecb170613d833
jsugarman <joel.sugarman@digital.justice.gov.uk>
2018-07-30 11:21:30 +0100

    Fix rubocop violations

--- spec/support/matchers/be_string_number.rb
+++ spec/support/matchers/be_string_number.rb
@@ -6,13 +6,13 @@ require 'bigdecimal/util'
 RSpec::Matchers.define :be_string_number do
   match do |actual|
     begin
-      BigDecimal.new(actual)
-    rescue
+      BigDecimal(actual)
+    rescue StandardError
       false
     end
   end
 
   description do
-    "be convertable to big decimal"
+    'be convertable to big decimal'
   end
 end
