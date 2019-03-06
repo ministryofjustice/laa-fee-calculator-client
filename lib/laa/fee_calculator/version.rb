bfe09bacc5fbcc608ec4367fa4b67948cdea9397
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-03-06 21:58:19 +0000

    Bump gem version and release date
    Add patch version bump - no
    functional changes bar rubocop
    violation fixes which should have no
    impact on use.

--- lib/laa/fee_calculator/version.rb
+++ lib/laa/fee_calculator/version.rb
@@ -2,8 +2,8 @@
 
 module LAA
   module FeeCalculator
-    VERSION = "0.2.0"
-    VERSION_RELEASED = "2018-09-18"
+    VERSION = "0.2.1"
+    VERSION_RELEASED = "2019-03-12"
     USER_AGENT = "laa-fee-calculator-client/#{VERSION}"
   end
 end
