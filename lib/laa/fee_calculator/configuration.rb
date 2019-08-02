4fbb9da46ca781c792af97dc24df71dc430a9695
jsugarman <joel.sugarman@digital.justice.gov.uk>
2019-08-02 10:11:13 +0100

    Modify default host
    Change to use the new live-1 hosted api domain
    name.

--- lib/laa/fee_calculator/configuration.rb
+++ lib/laa/fee_calculator/configuration.rb
@@ -3,7 +3,7 @@
 module LAA
   module FeeCalculator
     class Configuration
-      LAA_FEE_CALCULATOR_API_V1 = 'https://laa-fee-calculator-production.apps.cloud-platform-live-0.k8s.integration.dsd.io/api/v1'
+      LAA_FEE_CALCULATOR_API_V1 = 'https://laa-fee-calculator.service.justice.gov.uk/api/v1'
       HEADERS = { 'Accept' => 'application/json', 'User-Agent' => USER_AGENT }.freeze
 
       attr_accessor :host, :headers
